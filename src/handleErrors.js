3076c3d22582c62b0fd542e5bb3a96c95ce0d1db
MARIANOSDEV <nmkilmes@gmail.com>
2023-06-20 15:36:06 -0300

    Server - mongoDB fixed routes

--- src/handleErrors.js
+++
@@ -1,8 +0,0 @@
-module.exports = ((err, req, res, next) => {
-    console.error(err.stack)
-    if (err.name === 'CastError') {
-      res.status(422).send({error: "id used is malformed"})
-    }
-    else
-    res.status(503).send({error: err.message})
-  })
